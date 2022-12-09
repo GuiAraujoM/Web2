76049b14e668101e2971cf5a4187754cc416aebb
GuiAraujoM <garaujomedeiros.14@gmail.com>
2022-12-08 23:55:39 -0300

    fix required na category

--- Trabalho3/src/validators/TaskValidator.js
+++ Trabalho3/src/validators/TaskValidator.js
@@ -4,7 +4,7 @@ const schema = Joi.object({
   subject: Joi.string().min(3).max(30).required(),
   description: Joi.string().max(255),
   owner: Joi.string().pattern(new RegExp("^[0-9]$")).required(),
-  category: Joi.string().pattern(new RegExp("^[0-9]$")).required(),
+  category: Joi.string().pattern(new RegExp("^[0-9]$")),
   done: Joi.boolean().required(),
   due_date: Joi.date(),
   completed_at: Joi.date(),
